d964156d6f32e6c6dce0170c3bca073e7ccfbe5c
keepingoner <keepingoner@163.com>
2018-11-07 23:51:55 +0800

    tree

--- double_color/class_model_knn.py
+++ double_color/class_model_knn.py
@@ -9,6 +9,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
 
 
 data = pd.read_csv("data/2015001.csv")
@@ -18,23 +19,27 @@ X = data.iloc[0: -1, 1:7]
 Y = data.iloc[1:, 7:]
 
 # print(X.shape, Y.shape)
-
+minmax = MinMaxScaler()
+X = minmax.fit_transform(X)
 #测试集 训练集划分
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
 # 建模
 model = KNeighborsClassifier()
 # 交叉验证 选取最好 n
-n_neighbors = [2, 3, 4, 5]
+n_neighbors = [19]
 param = {"n_neighbors": n_neighbors}
-gc = GridSearchCV(model, param_grid=param)
+gc = GridSearchCV(model, param_grid=param, cv=3)
 gc.fit(x_train, y_train)
 
 # 预测准确率
 print(gc.score(x_test, y_test))
+
+print(gc.)
+
 # 交叉验证中最好的结果
 print(gc.best_score_)
 # 最好的模型
-# print(gc.best_estimator_)
+print(gc.best_estimator_)
 # 每个k的 验证结果
-print(gc.cv_results_)
+# print(gc.cv_results_)
