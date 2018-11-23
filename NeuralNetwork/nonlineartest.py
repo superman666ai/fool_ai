6a6aafd79ec865b5c5fd82c8443f1149a51e8373
keepingoner <keepingoner@163.com>
2018-11-23 13:40:38 +0800

    modi

--- NeuralNetwork/nonlineartest.py
+++ NeuralNetwork/nonlineartest.py
@@ -1,6 +1,6 @@
+
 from NeuralNetwork import NeuralNetwork
 import numpy as np
-
 nn = NeuralNetwork([2, 2, 1], 'tanh')
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([0, 1, 1, 0])
--- nn/test.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# @Time    : 2018/11/22 9:14
-# @Author  : jian
-# @File    : test.py
